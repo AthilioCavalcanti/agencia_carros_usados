f2645fc097915bf5a6b088fe0f5ce1801e7f6dd4
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2024-01-16 15:49:01 -0300

    Adiciona atributos e assinatura de métodos da classe Agencia

--- agencia.py
+++ agencia.py
@@ -1,3 +1,26 @@
+from typing import List
+from carro import Carro
+from transacao import Transacao
+from cliente import Cliente
+
 class Agencia:
-    def __init__(self) -> None:
+    def __init__(self, nome: str) -> None:
+        self.__nome: str = nome
+        self._carros: List[Carro] = []
+        self._transacoes: List[Transacao]
+    
+    @property
+    def nome(self) -> str:
+        return self.__nome
+    
+    def adiciona_carro(self, carro: Carro) -> None:
+        pass
+
+    def vende_carro(self, carro: Carro, cliente: Cliente) -> None:
+        pass
+
+    def exibi_carros(self) -> None:
+        pass
+
+    def exibi_transacoes(self) -> None:
         pass
