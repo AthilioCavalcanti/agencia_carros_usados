eca9ba1efbcdc4250d482b852228c550bda635a8
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2024-01-16 16:02:02 -0300

    Adiciona atributos da classe Cliente

--- cliente.py
+++ cliente.py
@@ -1,3 +1,5 @@
 class Cliente:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, nome: str, endereco: str, telefone: str) -> None:
+        self.nome = nome
+        self.endereco = endereco
+        self.telefone = telefone
