fe94feeff3ccebbefe2f713f37f05cd6af6ff7c8
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2024-01-16 16:00:02 -0300

    Adiciona atributos e assinatura de métodos da classe Carro

--- carro.py
+++ carro.py
@@ -1,3 +1,24 @@
 class Carro:
-    def __init__(self) -> None:
+    def __init__(
+            self, 
+            modelo: str, 
+            marca: str, 
+            tipo_carroceria: str, 
+            ano: int, 
+            cor: str, 
+            preco: float, 
+            quilometragem: int
+        ) -> None:
+        self.modelo = modelo
+        self.marca = marca
+        self.tipo_carroceria = tipo_carroceria
+        self.ano = ano
+        self.cor = cor
+        self.preco = preco
+        self.quilometragem = quilometragem
+
+    def exibir_detalhes(self) -> None:
+        pass
+
+    def calcular_depreciacao(self) -> None:
         pass
