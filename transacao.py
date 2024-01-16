f5d9c59c50570c5d68c96bd5deb2e31199be0b29
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2024-01-16 16:07:52 -0300

    Adiciona atributos da classe Transacao

--- transacao.py
+++ transacao.py
@@ -1,3 +1,10 @@
+from datetime import datetime
+from carro import Carro
+from cliente import Cliente
+
 class Transacao:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, cliente: Cliente, carro: Carro, valor: float, data: datetime) -> None:
+        self.cliente = cliente
+        self.carro = carro
+        self.valor = valor
+        self.data = data
