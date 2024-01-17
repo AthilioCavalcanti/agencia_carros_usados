43319d8328a749a3d241caa677f2d9f5c34c711c
Athílio Cavalcanti <athiliocavalcanti@gmail.com>
2024-01-17 18:54:31 -0300

    Atualiza classe Cliente

--- cliente.py
+++ cliente.py
@@ -1,5 +1,23 @@
 class Cliente:
     def __init__(self, nome: str, endereco: str, telefone: str) -> None:
-        self.nome = nome
-        self.endereco = endereco
-        self.telefone = telefone
+        self._nome = nome
+        self._endereco = endereco
+        self._telefone = telefone
+
+    @property
+    def nome(self) -> str:
+        return self._nome
+    
+    @property
+    def endereco(self) -> str:
+        return self._endereco
+    
+    @property
+    def telefone(self) -> str:
+        return self._telefone
+    
+    def atualizar_endereco(self, novo_endereco: str) -> None:
+        self._endereco = novo_endereco
+    
+    def atualizar_telefone(self, novo_numero: str) -> None:
+        self._telefone = novo_numero
